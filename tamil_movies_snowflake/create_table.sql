6ccbd44bc2a7f14e25dd76778a86707cf5e40883
abitofabi <72492983+abitofabi@users.noreply.github.com>
2025-06-12 21:44:58 -0600

    Create create_table.sql

---
+++ tamil_movies_snowflake/create_table.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TABLE tamil_movies (
+    MovieName STRING,
+    Genre STRING,
+    Rating FLOAT,
+    Director STRING,
+    Actor STRING,
+    PeopleVote INT,
+    Year INT,
+    Hero_Rating FLOAT,
+    movie_rating FLOAT,
+    content_rating STRING
+);
